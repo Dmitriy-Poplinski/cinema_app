61c9deacdcce31e9fc8a6186dca2f9d8b1cde0e4
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-18 13:43:38 +0300

    Realized based template of Header component

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -2,6 +2,24 @@ import classes from './Header.module.css'
 
 export function Header () {
     return (
-        <header className={classes.header}>Header</header>
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                <span>Cinema App</span>
+            </div>
+            <div className={classes.headerMenu}>
+                <div>
+                    Фільми сьогодні
+                </div>
+                <div>
+                    Фільми завтра
+                </div>
+                <div>
+                    Вибрати дату
+                </div>
+                <div>
+                    Про нас
+                </div>
+            </div>
+        </header>
     )
 }
