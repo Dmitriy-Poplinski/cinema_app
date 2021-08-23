a956b606e072c1e06f196c6548d77032a3fccfc1
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-23 15:27:23 +0300

    Added Datepicker from MaterialUI

---
+++ src/componets/DatePicker.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import Grid from '@material-ui/core/Grid'
+import MomentUtils from '@date-io/moment'
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers'
+
+export const MaterialUIPickers = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date('2021-08-23T21:11:54'))
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date)
+  }
+
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  )
+}
--- src/componets/ModalDate.jsx
+++ src/componets/ModalDate.jsx
@@ -1,4 +1,5 @@
-import { Modal, Button, FloatingLabel, Form } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
+import { MaterialUIPickers } from './DatePicker';
 
 export const ModalDate = (props) => (
     <>
@@ -7,17 +8,7 @@ export const ModalDate = (props) => (
                 <Modal.Title>Підберіть дату</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <FloatingLabel controlId="floatingSelect" label="Works with selects">
-                    <Form.Select aria-label="Floating label select example">
-                        <option>Оберіть доступну дату</option>
-                        <option value="24.08.2021">24.08.2021</option>
-                        <option value="25.08.2021">25.08.2021</option>
-                        <option value="26.08.2021">26.08.2021</option>
-                        <option value="27.08.2021">27.08.2021</option>
-                        <option value="28.08.2021">28.08.2021</option>
-                        <option value="29.08.2021">29.08.2021</option>
-                    </Form.Select>
-                </FloatingLabel>
+                <MaterialUIPickers />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={props.handleClose}>
